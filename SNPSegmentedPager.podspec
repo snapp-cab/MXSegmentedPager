0e9aebb0cf63a4d025f83123f4f51a296f214543
farhad <jebelli.farhad@gmail.com>
2018-10-03 13:24:55 +0330

    SNPSegmentedPager

---
+++ SNPSegmentedPager.podspec
@@ -0,0 +1,44 @@
+#
+# Be sure to run `pod lib lint SNPSegmentedPager.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'SNPSegmentedPager'
+  s.version          = '0.1.1'
+  s.summary          = 'Segmented pager view with Parallax header.'
+
+  s.description      = <<-DESC
+  MXSegmentedPager combines [MXPagerView](https://github.com/maxep/MXPagerView) with [HMSegmentedControl](https://github.com/HeshamMegid/HMSegmentedControl) to control the page selection.
+  The integration of [MXParallaxHeader](https://github.com/maxep/MXParallaxHeader) allows you to add an parallax header on top while keeping a reliable scrolling effect.
+  DESC
+# This description is used to generate tags and improve search results.
+#   * Think: What does it do? Why did you write it? What is the focus?
+#   * Try to keep it short, snappy and to the point.
+#   * Write the description between the DESC delimiters below.
+#   * Finally, don't worry about the indent, CocoaPods strips it!
+
+  s.homepage         = 'https://github.com/snapp-cab/SNPSegmentedPager'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'jebelli.farhad@gmail.com' => 'jebelli.farhad@gmail.com' }
+  s.source           = { :git => 'https://github.com/snapp-cab/SNPSegmentedPager.git', :tag => s.version.to_s }
+  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+  s.swift_version = '4.2'
+  s.platform     = :ios, '9.0'
+  s.ios.deployment_target = '9.0'
+  s.requires_arc = true
+  s.source_files = ['SNPSegmentedPager/Classes/**/*.{swift}','SNPSegmentedPager/Classes/**/*.{m}','SNPSegmentedPager/Classes/**/*.{h}']
+  s.public_header_files = ['SNPSegmentedPager/**/*.{h}']
+  s.dependency 'MXPagerView', '0.2.1' 
+  s.dependency 'MXParallaxHeader', '0.6.1'
+  
+  # s.resource_bundles = {
+  #   'SNPSegmentedPager' => ['SNPSegmentedPager/Assets/*.png']
+  # }
+
+
+end
---
+++ SNPSegmentedPager/Classes/ViewSegmentedControll/ViewSegmentControll.swift
@@ -0,0 +1,129 @@
+//
+//  ViewSegmentControll.swift
+//  MXSegmentedPager
+//
+//  Created by farhad jebelli on 6/17/18.
+//  Copyright © 2018 maxep. All rights reserved.
+//
+
+import UIKit
+
+@objc open class ViewSegmentControll: UIControl {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var indicatorColor:[UIColor]?
+    var indicatorHeigth: CGFloat = 0
+    @objc open var selectedSegmentIndex:Int = 0 {
+        didSet{
+            for (index,view) in tabViews?.enumerated() ?? [].enumerated(){
+                if(index != selectedSegmentIndex){
+                    view.segmentViewIsSelected = false
+                }else {
+                    view.segmentViewIsSelected = true
+                }
+            }
+        }
+    }
+    var tabViews:[SegmentedView]? {
+        didSet{
+            for (index,view) in tabViews?.enumerated() ?? [].enumerated(){
+                if(index != selectedSegmentIndex){
+                    view.segmentViewIsSelected = false
+                }else {
+                    view.segmentViewIsSelected = true
+                }
+            }
+        }
+    }
+    var indicator:UIView?
+    var indicatorLeading: NSLayoutConstraint?
+    
+    
+    
+    @objc open func setViews(_ views: [SegmentedView],heigth: CGFloat,selectedSegmentIndex: Int){
+        for view in tabViews ?? [] {
+            view.removeFromSuperview()
+        }
+        
+        self.tabViews = views
+        self.selectedSegmentIndex = selectedSegmentIndex
+        indicatorHeigth = heigth
+        views.forEach(addSubview(_:))
+        for (offset,view) in views.enumerated() {
+            view.index = offset
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.leftAnchor.constraint(equalTo: { () -> NSLayoutXAxisAnchor in if offset == 0 {return self.leftAnchor} else {return views[offset-1].rightAnchor}}()).isActive = true
+            view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            view.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -indicatorHeigth).isActive = true
+            if offset == views.count-1 {
+                view.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+            }else {
+                view.widthAnchor.constraint(equalTo: views[views.count-1].widthAnchor).isActive = true
+            }
+            view.addTarget(self, action: #selector(pageSelected), for: UIControlEvents.touchUpInside)
+            
+        }
+        
+        if views.count > 0 {
+            if indicator == nil {
+                indicator = UIView();
+                addSubview(indicator!)
+            }
+            indicator?.backgroundColor = indicatorColor?[selectedSegmentIndex] ?? UIColor.black
+            indicator?.translatesAutoresizingMaskIntoConstraints = false
+            
+            indicator?.heightAnchor.constraint(equalToConstant: indicatorHeigth).isActive = true
+            indicator?.widthAnchor.constraint(equalTo: views[0].widthAnchor).isActive = true
+            indicator?.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            indicatorLeading = indicator?.leftAnchor.constraint(equalTo: tabViews![selectedSegmentIndex].leftAnchor)
+            indicatorLeading?.isActive = true
+
+
+        }
+
+        
+    }
+    
+    @objc open func setIndocatorColor(colors: [UIColor]){
+        self.indicatorColor = colors
+        indicator?.backgroundColor = colors[selectedSegmentIndex % colors.count];
+    }
+    
+    
+    @objc func pageSelected(control: SegmentedView){
+        selectedSegmentIndex = control.index!
+        sendActions(for: .valueChanged);
+    }
+    
+    @objc func setSelectedPage(index: Int){
+        selectedSegmentIndex = index
+        sendActions(for: .valueChanged)
+    }
+    
+    @objc open func setSelectedSegmentIndex(_ index: Int, animated: Bool){
+
+        
+        self.layoutIfNeeded()
+        UIView.animate(withDuration: 0.3, animations: {[unowned self] in
+        self.indicatorLeading?.isActive = false
+        self.indicator?.removeConstraint(self.indicatorLeading!)
+        self.indicatorLeading = self.indicator?.leftAnchor.constraint(equalTo: self.tabViews![index].leftAnchor)
+        self.indicatorLeading!.isActive = true
+        self.selectedSegmentIndex = index
+        self.indicator?.backgroundColor = self.indicatorColor?[index]
+        self.layoutIfNeeded()
+        })
+        
+    }
+    
+    
+    
+    
+    
+
+}
